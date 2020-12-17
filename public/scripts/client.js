e4007cb9ce41fd55609703521177c8841b5033e8
luke-berzins <lukeberzins16@gmail.com>
2020-12-17 02:11:57 +0000

    dynamic tweets functionality incorporated with other files

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,35 +1,5 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 $(document).ready(function() {
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
+  
 const renderTweets = function(tweets) {
   for (const twit of tweets) {
     $('#tweets-container').prepend(createTweetElement(twit));
@@ -50,11 +20,12 @@ const createTweetElement = function(tweet) {
         <span class="username"><strong>${tweetUser}</strong></span>
       </div>
 
-        <span class="end-element">${tweetHandle}</span>
+        <span id="handle" class="end-element">${tweetHandle}</span>
      </header>
           <span id="tweet-content">${tweetContent}</span>
 
     <hr class="lineBreak">
+
      <footer class="tweets-footer">
         <span>Posted <div id="age-of-tweet">${tweetAge}</div></span>
         <div class="tweet-buttons">
@@ -67,5 +38,38 @@ const createTweetElement = function(tweet) {
   return $tweet;
 }
 
-renderTweets(data)
+ const loadTweets = function() {
+$.ajax('tweets', { 
+  method: 'GET'
+  })
+  .then(function (tweets) {
+    renderTweets(tweets);
+  });
+};
+loadTweets();
+$("#tweet-form").on("submit", function(event) {
+  event.preventDefault();
+  const tweetContent = $("#tweet-text").val();
+  if (0 < tweetContent.length && tweetContent.length <= 140) {
+    $("#size-error").addClass("error-message")
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      data: {text: tweetContent}
+    }).then(function () {
+      $("#tweet-text").val('')
+      loadTweets();
+    });
+    loadTweets();
+  } else if (tweetContent.length > 240) {
+    $("#size-error").removeClass("error-message");
+    $("#size-error").text("Too Long");
+  } else {
+    $("#size-error").removeClass("error-message");
+    $("#size-error").text("Error no content");
+  }
+})
+
+
+  
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,10 @@
 $(document).ready(function() {
   
-  
-  $(".tweets-body").hover(function() {
-    $(this).toggleClass("mouse-on-tweet")
-    $(".end-element").toggle("end-element");
-  })
-
-
-
- 
   $("#tweet-text").keyup(function() {
     if (event.keyCode == 13) {
       event.preventDefault();
     }
     let maxLength= 140;
-    console.log($(this).val().length)
     let textlen = maxLength - $(this).val().length;
     $( ".tweeting" ).children(".counter").text(textlen);
     if (textlen < 0) {
