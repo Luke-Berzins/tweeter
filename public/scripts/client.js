67b8aa11d2214d4acc6a75a98c2b9dedb0094051
luke-berzins <lukeberzins16@gmail.com>
2020-12-18 04:48:38 +0000

    debugging and code cleanup

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,24 +1,47 @@
 $(document).ready(function() {
-const escape =  function(str) {
-  let div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-}
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+  const renderTweets = function(tweets) {
+    for (const twit of tweets) {
+      $('#tweets-container').prepend(createTweetElement(twit));
+    }
+  };
 
-  
-const renderTweets = function(tweets) {
-  for (const twit of tweets) {
-    $('#tweets-container').prepend(createTweetElement(twit));
-  }
-}
+  const timePassed = (timePosted) => {
+    let current = Date.now();
+    let time = current - timePosted;
+    const seconds = Math.floor(time / 1000);
+    const minutes = Math.floor(time / (1000 * 60));
+    const hours = Math.floor(time / (1000 * 60 * 60));
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const months = Math.floor(time / (1000 * 60 * 60 * 24 * 31));
+    const years = Math.floor(time / (1000 * 60 * 60 * 24 * 12));
+    if (seconds < 60) {
+      return seconds + " seconds";
+    } else if (minutes < 60) {
+      return minutes + " minutes";
+    } else if (hours < 24) {
+      return hours + " hours";
+    } else if (days < 31) {
+      return days + " days";
+    } else if (months < 12) {
+      return months + " months";
+    } else {
+      return years + " years";
+    }
+  };
 
-const createTweetElement = function(tweet) {
-  const userImage = tweet["user"]["avatars"]
-  const tweetUser = escape(tweet["user"]["name"])
-  const tweetHandle = escape(tweet["user"]["handle"])
-  const tweetContent = escape(tweet["content"]["text"]);
-  const tweetAge = new Date(tweet["created_at"]).toLocaleString();
-  let $tweet = `
+
+  const createTweetElement = function(tweet) {
+    const userImage = tweet["user"]["avatars"];
+    const tweetUser = escape(tweet["user"]["name"]);
+    const tweetHandle = escape(tweet["user"]["handle"]);
+    const tweetContent = escape(tweet["content"]["text"]);
+    const tweetAge = timePassed(tweet["created_at"]);
+    let $tweet = `
   <article class="tweets-body" >
     <header class="tweets-header">
       <div class="left-name">
@@ -33,49 +56,50 @@ const createTweetElement = function(tweet) {
     <hr class="lineBreak">
 
      <footer class="tweets-footer">
-        <span>Posted <div id="age-of-tweet">${tweetAge}</div></span>
+        <span>Posted <div id="age-of-tweet">${tweetAge} ago</div></span>
         <div class="tweet-buttons">
-        <i class="fas fa-flag"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="fas fa-heart" ></i>
+        <i id="flag-tweet" class="fas fa-flag"></i>
+        <i id="re-tweet" class="fas fa-retweet"></i>
+        <i id="heart-tweet" class="fas fa-heart" ></i>
         </div>
       </footer>
-    </article>`
-  return $tweet;
-}
+    </article>`;
+    return $tweet;
+  };
 
- const loadTweets = function() {
-$.ajax('tweets', { 
-  method: 'GET'
-  })
-  .then(function (tweets) {
-    renderTweets(tweets);
+  const loadTweets = function() {
+    $.ajax('tweets', {
+      method: 'GET'
+    })
+      .then(function(tweets) {
+        $('#tweets-container').empty();
+        renderTweets(tweets);
+      });
+  };
+  loadTweets();
+
+
+  $("#tweet-form").on("submit", function(event) {
+    event.preventDefault();
+    let tweetContent = $("#tweet-text").val();
+    let tweetSize = tweetContent.length;
+    if (tweetSize > 140) {
+      $("#size-error").removeClass("error-message");
+      $("#size-error").text("Too Long");
+    } else if (tweetSize === 0) {
+      $("#size-error").removeClass("error-message");
+      $("#size-error").text("Error no content");
+    } else {
+      $("#size-error").addClass("error-message");
+      $.ajax({
+        method: "POST",
+        url: "/tweets",
+        data: {text: tweetContent}
+      }).then(function() {
+        $("#tweet-text").val('');
+        $(".tweeting").children(".counter").text("140");
+        loadTweets();
+      });
+    }
   });
-};
-loadTweets();
-
-
-$("#tweet-form").on("submit", function(event) {
-  event.preventDefault();
-  let tweetContent = $("#tweet-text").val();
-  let tweetSize = tweetContent.length;
-  if (tweetSize > 140) {
-    $("#size-error").removeClass("error-message");
-    $("#size-error").text("Too Long");
-  } else if (tweetSize === 0) {
-    $("#size-error").removeClass("error-message");
-    $("#size-error").text("Error no content");
-  } else {
-    $("#size-error").addClass("error-message");
-    $.ajax({
-      method: "POST",
-      url: "/tweets",
-      data: {text: tweetContent}
-    }).then(function () {
-      $("#tweet-text").val('')
-      loadTweets();
-    });
-    loadTweets();
-  } 
-})
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,21 +1,19 @@
 $(document).ready(function() {
 
-  $(".writeTweet").click(function () {
+  $(".writeTweet").click(function() {
     $("#new-tweet-form").slideToggle("fast");
-    $("#tweet-text").focus()
+    $("#tweet-text").focus();
   });
   
   $("#tweet-text").keyup(function() {
-    if (event.keyCode == 13) {
-      event.preventDefault();
-    }
-    let maxLength= 140;
+    let maxLength = 140;
     let textlen = maxLength - $(this).val().length;
-    $( ".tweeting" ).children(".counter").text(textlen);
+    $(".tweeting").children(".counter").text(textlen);
     if (textlen < 0) {
-      $( ".tweeting" ).children(".counter").addClass("over-limit");
+      // display a error, error type specified through the composer script
+      $(".tweeting").children(".counter").addClass("over-limit");
     } else {
-      $( ".tweeting" ).children(".counter").removeClass("over-limit");
+      $(".tweeting").children(".counter").removeClass("over-limit");
     }
   });
 });
